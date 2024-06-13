acd840dfa994296984e148737729bef11146eaa3
starberries <maistarberries@gmail.com>
2024-06-13 14:12:42 -0400

    Implement 5-round game function
    A parent function will run the game through fives
    rounds via a loop, determining the winner of each
    round.

--- files/script.js
+++ files/script.js
@@ -1,5 +1,36 @@
-let humanScore = 0
-let computerScore = 0
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice) {
+        let humanChoiceCase = humanChoice.toLowerCase();
+        if (humanChoiceCase != computerChoice) {
+            if (
+                (humanChoiceCase == 'rock' && computerChoice == 'scissors') ||
+                (humanChoiceCase == 'paper' && computerChoice == 'rock') ||
+                (humanChoiceCase == 'scissors' && computerChoice == 'paper')
+            ) {
+                console.log(`You win! ${humanChoiceCase} beats ${computerChoice}!`)
+                humanScore += 1
+            } else if (
+                (humanChoice == 'rock' && computerChoice == 'paper') ||
+                (humanChoice == 'paper' && computerChoice == 'scissors') ||
+                (humanChoice == 'scissors' && computerChoice == 'rock')
+            ) {
+                console.log(`You lose! ${computerChoice} beats ${humanChoiceCase}!`)
+                computerScore += 1
+            };
+        } else {
+            console.log(`You tied! ${humanChoiceCase} vs. ${computerChoice}!`)
+        };
+    };
+
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+}
 
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
@@ -16,25 +47,4 @@ function getComputerChoice() {
 function getHumanChoice() {
     let input = prompt('Choose rock, paper, or scissors.');
     return input
-};
-
-function playRound(humanChoice, computerChoice) {
-    let humanChoiceCase = humanChoice.toLowerCase();
-    if (humanChoiceCase != computerChoice) {
-        if (
-            (humanChoiceCase == 'rock' && computerChoice == 'scissors') ||
-            (humanChoiceCase == 'paper' && computerChoice == 'rock') ||
-            (humanChoiceCase == 'scissors' && computerChoice == 'paper')
-        ) {
-            console.log(`You win! ${humanChoiceCase} beats ${computerChoice}!`)
-        } else if (
-            (humanChoice == 'rock' && computerChoice == 'paper') ||
-            (humanChoice == 'paper' && computerChoice == 'scissors') ||
-            (humanChoice == 'scissors' && computerChoice == 'rock')
-        ) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoiceCase} !`)
-        };
-    } else {
-        console.log(`You tied! ${humanChoiceCase} vs. ${computerChoice}!`)
-    };
 };
