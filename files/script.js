30508402058688e8ef757ea835249c143acc34d4
starberries <maistarberries@gmail.com>
2024-06-13 12:57:36 -0400

    Code update
    Inconsistent and unreachable code removed in
    blocks for better readaibility and consistency.

--- files/script.js
+++ files/script.js
@@ -6,20 +6,17 @@ function getComputerChoice() {
     switch (rand) {
         case 0:
             return 'rock'
-            break;
         case 1:
             return 'paper'
-            break;
         case 2:
             return 'scissors'
-            break;
     }
 };
 
 function getHumanChoice() {
     let input = prompt('Choose rock, paper, or scissors.');
     return input
-}
+};
 
 function playRound(humanChoice, computerChoice) {
     let humanChoiceCase = humanChoice.toLowerCase();
