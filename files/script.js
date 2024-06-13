07ea6c75f6c2b389ed5712cc5900f26ec686fb08
starberries <maistarberries@gmail.com>
2024-06-13 12:23:06 -0400

    Implement player choice
    A function will will use a prompt to get the human
    player's selection for the game.

--- files/script.js
+++ files/script.js
@@ -2,14 +2,18 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
     switch (rand) {
         case 0:
-            return "Rock"
+            return 'rock'
             break;
         case 1:
-            return "Paper"
+            return 'paper'
             break;
         case 2:
-            return "Scissors"
+            return 'scissors'
             break;
     }
 };
 
+function getHumanChoice() {
+    let input = prompt('Choose rock, paper, or scissors.');
+    return input
+}
